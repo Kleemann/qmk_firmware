0cbd931f01cb6c2d7edab8d754621969937568bf
Kleemann <madskleemann@me.com>
2023-04-20 09:17:37 +0200

    magic split use I2C

--- keyboards/makle/magicsplit/config.h
+++ keyboards/makle/magicsplit/config.h
@@ -8,7 +8,12 @@
 #define MATRIX_ROWS 10
 #define MATRIX_COLS 10
 
+// Listen for an active USB communication 
+// to delegate which part is master and which is slave.
+// USB communication, means that half assumes it is the master, 
+// otherwise it assumes it is the slave.
 #define SPLIT_USB_DETECT
+
 #define DIODE_DIRECTION COL2ROW
 #define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
 #define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, NO_PIN, NO_PIN, NO_PIN }
@@ -17,8 +22,5 @@
 #define ENCODERS_PAD_A_RIGHT { F5 }
 #define ENCODERS_PAD_B_RIGHT { F4 }
 
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
-
-/* serial.c configuration for split keyboard */
-#define SOFT_SERIAL_PIN D0
+// Use I2C for split keyboard transport communication
+#define USE_I2C
